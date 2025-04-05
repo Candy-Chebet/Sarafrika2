4cb051bfab9e8c22881becbc5bb0ab3064b51282
Candy-Chebet <chebetcandychebet@gmail.com>
2025-04-05 12:11:56 +0300

    logo

--- src/containers/blog/Blog.jsx
+++ src/containers/blog/Blog.jsx
@@ -1,22 +1,22 @@
 import React from 'react';
 import Article from '../../components/article/Article';
-import { elimika, bff, blog03, blog04, blog05 } from './imports';
+import { elimika, bff } from './imports';
 import './blog.css';
-
+// , blog03, blog04, blog05 - add them to the elimika and bff list once apps are updated
 const Blog = () => (
   <div className="gpt3__blog section__padding" id="apps">
     <div className="gpt3__blog-heading">
       <h1 className="gradient__text">We are diverse <br /> Explore our various programs</h1>
     </div>
     <div className="gpt3__blog-container">
-      <div className="gpt3__blog-container_groupA">
+      {/* <div className="gpt3__blog-container_groupA">
         <Article imgUrl={elimika} text="Elimika deals with all things music and arts education. Get to be under the tutilage of renown names in the industry." />
-      </div>
+      </div> */}
       <div className="gpt3__blog-container_groupB">
         <Article imgUrl={bff} text="Bound For Fame exposes the artist to experience, then the market through networking and job opportunities." />
-        <Article imgUrl={blog03} text="App3" />
-        <Article imgUrl={blog04} text="App4" />
-        <Article imgUrl={blog05} text="App5" />
+        <Article imgUrl={elimika} text="Elimika deals with all things music and arts education. Get to be under the tutilage of renown names in the industry." />
+        {/* <Article imgUrl={blog04} text="App4" />
+        <Article imgUrl={blog05} text="App5" /> */}
       </div>
     </div>
   </div>
